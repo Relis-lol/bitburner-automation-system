464ee9616f36a3fea0bdeca3a73ce48fb847917d
Relis-lol <relislol@yahoo.com>
2026-04-20 18:52:45 +0200

    Update basic_hack.js

--- legacy/basic_hack.js
+++ legacy/basic_hack.js
@@ -4,10 +4,13 @@ export async function main(ns) {
 
     while (true) {
         if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target) + 2) {
+            // Lower security if it gets too high
             await ns.weaken(target);
         } else if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) * 0.9) {
+            // Pump up the cash if it's below 90%
             await ns.grow(target);
         } else {
+            // Money is good and security is low, let's steal some
             await ns.hack(target);
         }
 
