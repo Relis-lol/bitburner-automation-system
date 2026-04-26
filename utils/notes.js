673603e5df33c3b6106915440991f47802e21af1
Relis-lol <relislol@yahoo.com>
2026-04-26 10:03:10 +0200

    Update notes.js

--- utils/notes.js
+++ utils/notes.js
@@ -3,14 +3,23 @@ export async function main(ns) {
   ns.disableLog("ALL");
   ns.clearLog();
 
-  ns.tail();
-  ns.resizeTail(520, 420);
-
-  ns.print("=== IMPORTANT FACTION SERVERS ===");
+  ns.ui.openTail();
+  ns.ui.resizeTail(560, 620);
   ns.print("CSEC              -> CyberSec");
   ns.print("avmnite-02h       -> NiteSec");
   ns.print("I.I.I.I           -> The Black Hand");
   ns.print("run4theh111z      -> BitRunners");
 
   ns.print("");
+  ns.print("buy TOR");
+  ns.print("buy BruteSSH.exe");
+  ns.print("buy FTPCrack.exe");
+  ns.print("buy relaySMTP.exe");
+  ns.print("buy HTTPWorm.exe");
+  ns.print("buy SQLInject.exe");
+  ns.print("buy ServerProfiler.exe");
+  ns.print("buy AutoLink.exe");
+  ns.print("buy DeepscanV1.exe");
+  ns.print("buy DeepscanV2.exe");
+  ns.print("buy Formulas.exe");
 }
